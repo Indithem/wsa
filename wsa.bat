fc7a7efb0a827412e617391474c7db4eed5c0fa2
Ambarkar Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-04-15 02:39:31 +0530

    Add files via upload

---
+++ wsa.bat
@@ -0,0 +1,18 @@
+@echo off
+set "option=%1"
+set "apk=%2"
+
+if /i '%option%'=='-h' goto help
+adb connect 127.0.0.1:58526
+if /i '%option%'=='' goto help
+
+if /i "%option%"=="-s" adb -s 127.0.0.1:58526 shell monkey -p com.android.settings -c android.intent.category.LAUNCHER 1
+if /i '%option%'=='-install' adb -s 127.0.0.1:58526 install "%apk%"
+
+
+exit /b1
+:help
+echo A script made for me to use wsa things
+echo    -s                  open settings
+echo    -install            intall apk to wsa
+echo    -h                  print this help message
