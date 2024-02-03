88dc27545f5c118f9080d10b46dced4a476f07c5
Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2024-02-03 15:26:47 +0530

    i didn't know bat

--- wsa.bat
+++ wsa.bat
@@ -3,17 +3,23 @@ set "option=%1"
 set "apk=%2"
 
 if /i '%option%'=='-h' goto help
-if /i '%option%'=='-r' adb disconnect 127.0.0.1:58526
-adb connect 127.0.0.1:58526
+if /i '%option%'=='-r' goto restart
 if /i '%option%'=='' goto help
 
 if /i "%option%"=="-s" adb -s 127.0.0.1:58526 shell monkey -p com.android.settings -c android.intent.category.LAUNCHER 1
-if /i '%option%'=='-sh' adb connect 127.0.0.1:58526
-adb shell
+if /i '%option%'=='-sh' goto shell
 if /i '%option%'=='-install' adb -s 127.0.0.1:58526 install "%apk%"
 
+exit /b 1
+:restart
+adb disconnect 127.0.0.1:58526
+adb connect 127.0.0.1:58526
+exit /b 0
 
-exit /b1
+:shell
+adb connect 127.0.0.1:58526
+adb shell
+
 :help
 echo A script made for me to use wsa things
 echo    -s                  open settings
